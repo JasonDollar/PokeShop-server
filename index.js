1e73a74519109f9d8c916c5405441416f6a60f0f
JasonDollar <elrey33@gmail.com>
2019-06-17 11:58:11 +0200

    change port to env variable

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('db connected'))
   .catch(e => console.log(e))
 
-server.start({ port: 4000 }, () => {
+server.start({ port: process.env.PORT }, () => {
   console.log('started')
 }) 
 
