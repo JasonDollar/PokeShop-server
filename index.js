95c608b80694e5cec918d9ebc3e1a9172ebe8a3c
JasonDollar <elrey33@gmail.com>
2019-10-29 11:33:42 +0100

    dependencies bump

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ server.express.use((req, res, next) => {
 
 
 mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, 
+  useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true
 })
   .then(() => console.log('db connected'))
   .catch(e => console.log(e))
